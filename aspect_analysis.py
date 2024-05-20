e576e953a575ab14fd38e5212e23dd5d0ca3b1d9
surya1321 <168954804+surya1321@users.noreply.github.com>
2024-05-20 20:08:40 +0530

    Add files via upload

---
+++ aspect_analysis.py
@@ -0,0 +1,105 @@
+# importing the required python libraries
+import pandas as pd
+import numpy as np
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer 
+
+import stanza
+
+def aspect_analysis(txt, stop_words, nlp):
+    
+    txt = txt.lower() # LowerCasing the given Text
+    sentList = nltk.sent_tokenize(txt) # Splitting the text into sentences
+
+    fcluster = []
+    totalfeatureList = []
+    finalcluster = []
+    dic = {}
+
+    for line in sentList:
+        newtaggedList = []
+        txt_list = nltk.word_tokenize(line) # Splitting up into words
+        taggedList = nltk.pos_tag(txt_list) # Doing Part-of-Speech Tagging to each word
+
+        newwordList = []
+        flag = 0
+        for i in range(0,len(taggedList)-1):
+            if(taggedList[i][1]=="NN" and taggedList[i+1][1]=="NN"): # If two consecutive words are Nouns then they are joined together
+                newwordList.append(taggedList[i][0]+taggedList[i+1][0])
+                flag=1
+            else:
+                if(flag==1):
+                    flag=0
+                    continue
+                newwordList.append(taggedList[i][0])
+                if(i==len(taggedList)-2):
+                    newwordList.append(taggedList[i+1][0])
+
+        finaltxt = ' '.join(word for word in newwordList) 
+        new_txt_list = nltk.word_tokenize(finaltxt)
+        wordsList = [w for w in new_txt_list if not w in stop_words]
+        taggedList = nltk.pos_tag(wordsList)
+        print(taggedList)
+        doc = nlp(finaltxt) # Object of Stanford NLP Pipeleine
+        
+        # Getting the dependency relations between the words
+        dep_node = []
+        for dep_edge in doc.sentences[0].dependencies:
+            dep_node.append([dep_edge[2].text, dep_edge[0].id, dep_edge[1]])
+
+        # Coverting it into appropriate format
+        for i in range(0, len(dep_node)):
+            if (int(dep_node[i][1]) != 0):
+                dep_node[i][1] = newwordList[(int(dep_node[i][1]) - 1)]
+
+        featureList = []
+        categories = []
+        for i in taggedList:
+            if(i[1]=='JJ' or i[1]=='NN' or i[1]=='JJR' or i[1]=='NNS' or i[1]=='RB'):
+                featureList.append(list(i)) # For features for each sentence
+                totalfeatureList.append(list(i)) # Stores the features of all the sentences in the text
+                categories.append(i[0])
+
+        for i in featureList:
+            filist = []
+            for j in dep_node:
+                if((j[0]==i[0] or j[1]==i[0]) and (j[2] in ["nsubj", "acl:relcl", "obj", "dobj", "agent", "advmod", "amod", "neg", "prep_of", "acomp", "xcomp", "compound"])):
+                    if(j[0]==i[0]):
+                        filist.append(j[1])
+                    else:
+                        filist.append(j[0])
+            fcluster.append([i[0], filist])
+            
+    for i in totalfeatureList:
+        dic[i[0]] = i[1]
+    
+    for i in fcluster:
+        s = [i[0],i[1][0]]
+        pos = nltk.pos_tag(s)
+        print(pos)
+        temp = []
+        for x in pos:
+            if "NN" in x:
+                temp.insert(0,x)
+            else:
+                temp.append(x)
+        if temp[0][1] == "NN":
+            finalcluster.append((temp[0][0],temp[1][0]))
+    fin = [] 
+    for i in finalcluster:
+        if i not in fin:
+            fin.append(i)
+    return(fin)
+
+# nlp = stanza.Pipeline()
+# stop_words = set(stopwords.words('english'))
+# # txt = "The food is great but the service is very bad."
+# txt = "the fish is great but the taste is very bad"
+
+# print(aspect_analysis(txt, stop_words, nlp))
+
+
---
+++ com_install.py
@@ -0,0 +1,10 @@
+# downloading necessary packages of nltk
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('vader_lexicon')
+
+# downloading English model and other essential tools for stanza     
+import stanza
+#stanza.download('en')
